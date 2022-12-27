4eac34c2a951e1995e2d145f539495f5a3db0b9d
gokulkar879 <gokulkarki987654@gmail.com>
2022-12-28 00:11:58 +0530

    added all the required functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useAppContext } from './context';
 
 function App() {
+  const { name, message, setName, setMessage, coffees, buyCoffee, withdrawAllTips } = useAppContext();
+
+  const handleClick = e => {
+    e.preventDefault();
+    buyCoffee();
+  }
+
+
+  const handleRecieve = e => {
+       withdrawAllTips();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleRecieve}>Recieve</button>
+        <form>
+          <label htmlFor='name'>Name</label>
+          <input 
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor='message'>Message</label>
+          <input
+          name="message"
+           value={message}
+           onChange={(e) => setMessage(e.target.value)}
+           />
+          <button onClick={handleClick}>Send</button>
+        </form>
+        <div className='coffees'>
+          <div className='coffees_center'>
+          {
+            coffees.map(coffee => {
+                const { from, timestamp, name, message } = coffee;
+                // console.log(from,timestamp._hex, name, message)
+                return <div key={timestamp} className="coffee">
+                  <p><span>From : </span>{from}</p>
+                  <p><span>Name : </span>{name}</p>
+                  <p><span>Message : </span>{message}</p>
+                </div>
+            })
+          }
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/context.js
@@ -0,0 +1,167 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {ethers} from 'ethers'
+import abi from './utils/contract.json';
+// import Web3 from 'web3';
+
+const abiAddress = "0xfaa2E9ac03ED5E835BA0Eb25E6B6DE176a0F3b54";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+   
+    const [coffees, setCoffees] = useState([]);
+    const [account, setAccount] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+
+    const isWalletConnected = async () => {
+        const {ethereum} = window;
+        console.log(ethereum);
+
+        if(ethereum) {
+            let wallet = null;
+            try {
+                wallet = await ethereum.request({
+                    method: 'eth_requestAccounts'
+                })
+            } catch(err) {
+                console.log(err);
+            }
+            if(wallet.length) {
+                setAccount(wallet[0]);
+            } else {
+                console.log("please connect to metamask");
+            }
+        } else {
+            console.log("please download metamask");
+        }
+    }
+    
+    const listAllCoffees = async () => {
+        const {ethereum} = window;
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+
+        const deployed_contract = new ethers.Contract(
+            abiAddress,
+            abi["abi"],
+            signer
+        )
+
+        const txn = await deployed_contract.listAllCoffees();
+        console.log(txn)
+        setCoffees(txn)
+        // console.log(coffees)
+    }
+    
+    const withdrawAllTips = async () => {
+        const {ethereum} = window;
+        if(ethereum && account) {
+            const provider = new ethers.providers.Web3Provider(ethereum, "any");
+            const signer = provider.getSigner();
+    
+            const deployed_contract = new ethers.Contract(
+                abiAddress,
+                abi["abi"],
+                signer
+            )
+
+            const txn = await deployed_contract.withdrawTips();
+
+            await txn.wait();
+
+        }
+    }
+
+    const buyCoffee = async () => {
+        const {ethereum} = window;
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+
+        const deployed_contract = new ethers.Contract(
+            abiAddress,
+            abi["abi"],
+            signer
+        )
+        try {
+            const txn = await deployed_contract.buyCoffee(
+                name ? name : "Test",
+                message ? message : "Test",
+                {
+                    value: ethers.utils.parseEther("0.001")
+                }
+            );
+            await txn.wait();
+        } catch(err) {
+           console.log(err);
+        }
+
+       
+        setMessage("");
+        setName("");
+    }
+
+    useEffect(() => {
+        let buyMeACoffee = null;
+        const {ethereum} = window;
+         isWalletConnected();
+        if(ethereum) {
+            listAllCoffees();
+        }
+
+         const onNewCoffee = (from, timestamp, name, message) => {
+            console.log("p");
+            console.log(coffees, from);
+           setCoffees((prevState) => [...prevState, {
+            from,
+            timestamp: new Date(timestamp * 1000),
+            name,
+            message
+           }])
+         }
+
+
+
+         if(ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum, "any");
+            const signer = provider.getSigner();
+
+            buyMeACoffee = new ethers.Contract(
+                abiAddress,
+                abi["abi"],
+                signer
+            )
+
+            buyMeACoffee.on("newCoffee", onNewCoffee);
+         } else {
+            console.log("please install metamask");
+         }
+
+         return () => {
+            if(buyMeACoffee) {
+                buyMeACoffee.off("newCoffee", onNewCoffee);
+            }
+         }
+
+    }, [])
+    return <AppContext.Provider value={{
+        buyCoffee,
+        coffees,
+        name, 
+        setName,
+        message,
+        setMessage,
+        withdrawAllTips
+
+    }}>
+        {
+            children
+        }
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
+
+export {AppProvider}
